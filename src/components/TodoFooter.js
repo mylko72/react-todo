83ac818af23239379d713e05fcbff99c8681567f
Kwanoh Lee <mypmk@naver.com>
2024-07-18 12:03:52 +0900

    todo 수정시 포커스 적용

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './TodoFooter.module.css';
 
 export default function TodoFooter({ mode, onAdd, onEdit, onCancel, editItem }) {
   const [item, setItem] = useState('')
+
+  const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,13 +26,14 @@ export default function TodoFooter({ mode, onAdd, onEdit, onCancel, editItem }) 
   useEffect(() => {
     if(editItem.name !== ''){
       setItem(prev => editItem.name);
+      inputRef.current.focus();
     }
   }, [editItem])
 
   return (
     <div className={`${styles[mode]} ${styles.footer}`}>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <input type="text" value={item} onChange={e => setItem(prev => e.target.value)} />
+        <input type="text" value={item} ref={inputRef} onChange={e => setItem(prev => e.target.value)} />
         <button type="submit" className={Object.keys(editItem).length && styles.edit}>{!Object.keys(editItem).length ? 'Add' : 'Edit'}</button>
         {Object.keys(editItem).length > 0 && <button type="button" onClick={handleCancel}>Cancel</button>}
       </form>
